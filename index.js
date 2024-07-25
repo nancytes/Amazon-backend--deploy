7d32962c960c3f39126705b9878409df70088469
Nancy Tesfaye <nancy23tes@gmail.com>
2024-07-25 15:44:03 +0300

    backend deployment

---
+++ index.js
@@ -0,0 +1,42 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "success",
+  });
+});
+
+
+app.post("/payment/create", async(req, res)=>{
+    const total = parseInt(req.query.total);
+    if (total > 0) {
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: total,
+                currency: "usd",
+            });
+            res.status(200).json({
+                clientSecret: paymentIntent.client_secret,
+            });
+        } catch (error) {
+            console.error("Error creating payment intent:", error);
+            res.status(500).json({ error: "Failed to create payment intent" });
+        }
+    } else {
+        res.status(400).json({ error: "Total amount must be greater than zero" });
+    }
+    
+})
+
+app.listen(3000, (err)=>{
+    if(err) throw err;
+    else console.log("Server is running on port 3000, http://localhost:3000");
+
+})
